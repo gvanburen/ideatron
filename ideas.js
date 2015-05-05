0335b444e6458dd82cffb7556d29ca3a67f24d7d
Greg Van Buren <g.vanburen@gmail.com>
2015-05-05 16:54:41 -0700

    Almost working

--- ideas.js
+++ ideas.js
@@ -1,4 +1,4 @@
-angular.module('ideaTron',['ngRoute','ngAnimate'])
+angular.module('ideaTron',['ngRoute','ngAnimate','firebase'])
 	.constant('FIREBASE_URL', 'https://burning-heat-343.firebaseio.com/ideas')
 	.factory('firebaseReference', function(FIREBASE_URL){
 		return new Firebase( FIREBASE_URL );
@@ -9,33 +9,46 @@ angular.module('ideaTron',['ngRoute','ngAnimate'])
 			templateUrl: 'main.html',
 			controller: 'mainCtrl'
 		})
-		.when('/signup',{
-			templateUrl: 'signup.html',
-			controller: 'signupCtrl'
-		})
-		.when('/login',{
-			templateUrl: 'login.html',
-			controller: 'loginCtrl'
-		})
+		// .when('/signup',{
+		// 	templateUrl: 'signup.html',
+		// 	controller: 'signupCtrl'
+		// })
+		// .when('/login',{
+		// 	templateUrl: 'login.html',
+		// 	controller: 'loginCtrl'
+		// })
 		.when('/ideas',{
 			templateUrl: 'ideas.html',
 			controller: 'ideaCtrl'
 		})
 	}])
-	.controller('mainCtrl', ['$scope','$location', function($scope, $location){
-		$scope.logMeIn = function(){
-			var ref = new Firebase('https://burning-heat-343.firebaseio.com/ideas');//firebaseReference;
+	.controller('mainCtrl', ['$rootScope','$scope','$location','firebaseReference', function($rootScope, $scope, $location, firebaseReference){
+		var ref = firebaseReference;
+		$scope.login = function(){
 			ref.authWithOAuthPopup("github", function(error, authData) {
 				if (error) {
+					if (error.code === "TRANSPORT_UNAVAILABLE") {
+						ref.authWithOAuthRedirect("github", function(error){
+							console.log("Login Failed!", error);
+						});
+					} else {
 					console.log("Login Failed!", error);
+					}
 				} else {
-					console.log("Authenticated successfully with payload:", authData);
+					ref.child(authData.uid).set({
+						provider: authData.provider,
+						name: authData.github.displayName
+					});
+
+					$rootScope.userId = authData.uid;
 					$location.path('/ideas');
+					$scope.$apply();
+					console.log("Authenticated successfully with payload:", authData);
+					
 				}
 			});
 		}
-		$scope.logMeInAnon = function(){
-			var ref = new Firebase('https://burning-heat-343.firebaseio.com/ideas');//firebaseReference;
+		$scope.loginAnon = function(){
 			ref.authAnonymously(function(error, authData) {
 				if (error) {
 					console.log("Login Failed!", error);
@@ -73,15 +86,11 @@ angular.module('ideaTron',['ngRoute','ngAnimate'])
 	// 		});
 	// 	}
 	// }])
-	.controller('ideaCtrl',['$scope','$http', function($scope, $http){
+	.controller('ideaCtrl',['$rootScope','$scope','$http','firebaseReference', function($rootScope, $scope, $http, firebaseReference){
+		var userId = $rootScope.userId;
 		$scope.selectService = function(){
-			// var random = Math.floor(Math.random() * (217-$scope.serviceNumber));
-			// console.log($scope.serviceNumber);
-			// console.log(random);
-			// $http.get("http://apis.io/api/search?q=all&skip=" + random + "&limit=" + $scope.serviceNumber).success(function(data){
-			// 	$scope.ideas = data.data;
-			// 	console.log(data);
-			// })
+			var ref = firebaseReference;
+			var userRef = ref.child(userId);
 			$http.get("http://apis.io/api/search?q=all&limit=217").success(function(data){
 				var random = data;
 				$scope.ideas = [];
@@ -89,6 +98,13 @@ angular.module('ideaTron',['ngRoute','ngAnimate'])
 					var num = Math.floor(Math.random() * 217);
 					$scope.ideas.push(random.data[num]);
 				}
+				var recentIdeas = userRef.push($scope.ideas);
+				$scope.recentIdeas = recentIdeas.key();
+				console.log($scope.recentIdeas);
+				userRef.on("value", function(snapshot){
+				console.log(snapshot.val());
 			})
+			})
+
 		}	
 	}])
