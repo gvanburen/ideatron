ffe1b857e2e0cdadfe317b8218327492bb869599
Greg Van Buren <g.vanburen@gmail.com>
2015-05-13 21:02:18 -0700

    Additional features

--- ideas.js
+++ ideas.js
@@ -9,14 +9,10 @@ angular.module('ideaTron',['ngRoute','ngAnimate','firebase'])
 			templateUrl: 'main.html',
 			controller: 'mainCtrl'
 		})
-		// .when('/signup',{
-		// 	templateUrl: 'signup.html',
-		// 	controller: 'signupCtrl'
-		// })
-		// .when('/login',{
-		// 	templateUrl: 'login.html',
-		// 	controller: 'loginCtrl'
-		// })
+		.when('/about', {
+			templateUrl: 'about.html',
+			controller: 'mainCtrl'
+		})
 		.when('/ideas',{
 			templateUrl: 'ideas.html',
 			controller: 'ideaCtrl'
@@ -69,30 +65,61 @@ angular.module('ideaTron',['ngRoute','ngAnimate','firebase'])
 			});
 		}
 	}])
-	// .controller('signupCtrl', ['$scope', function($scope){
-	// }])
-	// .controller('loginCtrl', ['$scope', function($scope){
-	// }])
-	.controller('ideaCtrl',['$rootScope','$scope','$http','$location','firebaseReference', function($rootScope, $scope, $http, $location, firebaseReference){
+	.controller('ideaCtrl',['$rootScope','$scope','$http','$location','$window','firebaseReference', function($rootScope, $scope, $http, $location, $window, firebaseReference){
 		$scope.numbers = [1,2,3];
 		var ref = firebaseReference;
 		var authData = ref.getAuth();
+		console.log(authData);
 		if (authData){
 			$scope.userId = authData.uid;
 			var userId = $scope.userId;
 			if (userId.indexOf("anonymous:") > -1){
 				console.log('log in to see recent ideas');
+				$scope.loggedIn = false;
 			} else {
-				$scope.loggedIn = true;	
+				$scope.loggedIn = true;
 			}
 			
 		}
-		// console.log(userId);
+		//console.log(userId);
+		console.log(userRef);
 		var userRef = ref.child(userId);
-		userRef.once("value", function(snapshot){
-			$scope.prevIdeas = snapshot.val();
+		if (userId.indexOf("anonymous:") !== 0){
+			userRef.limitToLast(5).once("value", function(snapshot){
+				if(snapshot.val() == null){
+					$scope.limitedIdeas = 0;
+				} else {
+					$scope.prevIdeas = snapshot.val();
+					$scope.limitedIdeas = Object.keys($scope.prevIdeas).length;
+					console.log($scope.limitedIdeas);
+					$scope.$apply();
+				}
+			})
+		}
+		$scope.showMore = function(){
+			userRef.limitToLast(10).once("value", function(snapshot){
+				$scope.prevIdeas = snapshot.val();
+				$scope.$apply();
+			})
+		}
+		$scope.clearData = function(){
+			userRef.remove();
 			$scope.$apply();
-		})
+			userRef.once("value", function(snapshot){
+				if(snapshot.val() == null){
+					$scope.limitedIdeas = 0;
+				}
+				$scope.$apply();
+			})
+		}
+		$scope.deleteMe = function(o){
+			userRef.child(o).remove();
+			userRef.limitToLast(5).on("value", function(snapshot){
+				$scope.prevIdeas = snapshot.val();
+				$scope.limitedIdeas = Object.keys($scope.prevIdeas).length;
+				$scope.$apply();
+			})
+		}
 		$scope.selectService = function(){
 			$http.get("http://apis.io/api/search?q=all&limit=217").success(function(data){
 				var random = data;
@@ -102,9 +129,10 @@ angular.module('ideaTron',['ngRoute','ngAnimate','firebase'])
 					$scope.ideas.push(random.data[num]);
 				}
 				userRef.push($scope.ideas);
-				userRef.on("value", function(snapshot){
+				userRef.limitToLast(5).on("value", function(snapshot){
 					console.log(snapshot.val());
 					$scope.prevIdeas = snapshot.val();
+					$scope.limitedIdeas = Object.keys($scope.prevIdeas).length;
 					$scope.$apply();
 				})
 			})
@@ -112,6 +140,7 @@ angular.module('ideaTron',['ngRoute','ngAnimate','firebase'])
 		$scope.logOut = function(){
 			userRef.unauth();
 			$scope.loggedIn = false;
-			$location.path('/');
+			$window.location.assign('/');
 		}
+
 	}])
